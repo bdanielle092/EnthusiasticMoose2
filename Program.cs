d9753526b30b8c1ae6d15e08b1e1b3c41a533204
Faith Magras <bdanielle092@gmail.com>
2020-11-12 15:40:15 -0600

    phase7

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace EnthusiasticMoose2
 {
     class Program
     {
-      static void Main(string[] args)
+     static void Main(string[] args)
 {
     Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
     Console.WriteLine("--------------------------------------------");
@@ -15,6 +15,14 @@ namespace EnthusiasticMoose2
     MooseSays("I really am enthusiastic");
 
     // As a question
+    CanadaQuestion();
+    EnthusiasticQuestion();
+    LoveCSharpQuestion();
+    SecretQuestion();
+}
+
+static void CanadaQuestion()
+{
     bool isTrue = MooseAsks("Is Canada real?");
     if (isTrue)
     {
@@ -23,6 +31,45 @@ namespace EnthusiasticMoose2
     else
     {
         MooseSays("I  K N E W  I T !!!");
+    }
+}
+
+static void EnthusiasticQuestion()
+{
+    bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
+    if (isEnthusiastic)
+    {
+        MooseSays("Yay!");
+    }
+    else
+    {
+        MooseSays("You should try it!");
+    }
+}
+
+static void LoveCSharpQuestion()
+{
+    bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
+    if (doesLoveCSharp)
+    {
+        MooseSays("Good job sucking up to your instructor!");
+    }
+    else
+    {
+        MooseSays("You will...oh, yes, you will...");
+    }
+}
+
+static void SecretQuestion()
+{
+    bool isEverythingFine = MooseAsks("Do you want to know a secret?");
+    if (isEverythingFine)
+    {
+        MooseSays("ME TOO!!!! I love secrets...tell me one!");
+    }
+    else
+    {
+        MooseSays("Oh, no...secrets are the best, I love to share them!");
     }
 }
 
