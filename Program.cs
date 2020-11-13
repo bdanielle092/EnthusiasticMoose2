e18ec087693a9074a0ac1a5956cb269126398e58
Faith Magras <bdanielle092@gmail.com>
2020-11-13 11:59:57 -0600

    added a few more comments

--- Program.cs
+++ Program.cs
@@ -12,12 +12,13 @@ namespace EnthusiasticMoose2
     Console.WriteLine();
 
     // Let the moose speak!
-    // the MooseSays Method is use here to talk to the user
+    //calling the MooseSays method
+    // the MooseSays Method on lines 71-100 is use here to talk to the user
     MooseSays("H I, I'M  E N T H U S I A S T I C !");
     MooseSays("I really am enthusiastic");
 
-    // As a question
-    //Here we ask the questions and then response base on if its true or false 
+    // As a question. This is where we call the question Method
+    //Here we ask the questions and then response base on if its true or false from question lines 32-46 and mooseAsk lines 49-68 methods
    Question("Is Canada real? Really?", "It seems very unlikely.", "I  K N E W  I T !!!");
    Question("Are you enthusiastic?", "Yay!", "You should try it!");
    Question("Do you love C# yet?", "Good job sucking up to your instructor!","You will...oh, yes, you will...");
@@ -25,9 +26,9 @@ namespace EnthusiasticMoose2
 
 }
 
-//This is the question Method. There are three parameters. 
+//This is where we define the question Method. There are three parameters. 
 //line 31-41 is saying fist the moose asks a question, second clears the console, third take the response of yes or no
-//this takes us to lines 48-68
+//this takes us to lines 50-69
  static void Question(string question, string yes, string no)
         {
             bool isTrue = MooseAsks(question);
@@ -43,9 +44,9 @@ namespace EnthusiasticMoose2
             }
 
         }
-
+        // Defining the MooseSays Method
         //here we see the response. If the answer is left blank we ask the question again.
-        // if yes we return true and if no we return false. This take us to lines 20-23
+        // if yes we return true and if no we return false. This take us to lines 22-25
 static bool MooseAsks(string question)
 {
     Console.Write($"{question} (Y/N): ");
@@ -67,7 +68,7 @@ static bool MooseAsks(string question)
     }
 }
 
-//this is the MooseSays Method. this takes us to lines 16-17
+//this is the MooseSays Method. this takes us to lines 16-17. We use string interpolation for message
        static void MooseSays(string message)
 {
     Console.WriteLine($@"
@@ -101,3 +102,8 @@ static bool MooseAsks(string question)
 }
     }
 }
+
+//console is a object
+//.ReadLine is a method 
+//string is a type
+// question is a variable
