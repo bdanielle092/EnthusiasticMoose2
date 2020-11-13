2469630c69495eee8c5d04b680e9a5e8243e9169
Faith Magras <bdanielle092@gmail.com>
2020-11-13 11:07:08 -0600

    added comments that walks through the code

--- Program.cs
+++ Program.cs
@@ -6,22 +6,28 @@ namespace EnthusiasticMoose2
     {
      static void Main(string[] args)
 {
+    // welcomes users
     Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
     Console.WriteLine("--------------------------------------------");
     Console.WriteLine();
 
     // Let the moose speak!
+    // the MooseSays Method is use here to talk to the user
     MooseSays("H I, I'M  E N T H U S I A S T I C !");
     MooseSays("I really am enthusiastic");
 
     // As a question
+    //Here we ask the questions and then response base on if its true or false 
    Question("Is Canada real? Really?", "It seems very unlikely.", "I  K N E W  I T !!!");
    Question("Are you enthusiastic?", "Yay!", "You should try it!");
    Question("Do you love C# yet?", "Good job sucking up to your instructor!","You will...oh, yes, you will...");
    Question("Do you want to know a secret?","ME TOO!!!! I love secrets...tell me one!","Oh, no...secrets are the best, I love to share them!");
+
 }
 
-
+//This is the question Method. There are three parameters. 
+//line 31-41 is saying fist the moose asks a question, second clears the console, third take the response of yes or no
+//this takes us to lines 48-68
  static void Question(string question, string yes, string no)
         {
             bool isTrue = MooseAsks(question);
@@ -35,7 +41,11 @@ namespace EnthusiasticMoose2
                 Console.Clear();
                 MooseSays(no);
             }
+
         }
+
+        //here we see the response. If the answer is left blank we ask the question again.
+        // if yes we return true and if no we return false. This take us to lines 20-23
 static bool MooseAsks(string question)
 {
     Console.Write($"{question} (Y/N): ");
@@ -57,6 +67,7 @@ static bool MooseAsks(string question)
     }
 }
 
+//this is the MooseSays Method. this takes us to lines 16-17
        static void MooseSays(string message)
 {
     Console.WriteLine($@"
